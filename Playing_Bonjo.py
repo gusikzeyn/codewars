a708e70050a539984fa480ac7e89d3260cbe6170
gusikzeyn <huseynzeyn2009@gmail.com>
2024-01-16 12:29:58 +0400

    Create Playing_Bonjo.py
    My first Solution

---
+++ Playing_Bonjo.py
@@ -0,0 +1,8 @@
+def areYouPlayingBanjo(name):
+    if name[0].lower() == 'r':
+        print(name + ' plays banjo')
+    else:
+        print(name + ' does not play banjo')
+
+# Change Name to Test my Code
+areYouPlayingBanjo('Rfiq')

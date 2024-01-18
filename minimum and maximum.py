8604b7fcd29c6913bc4a3d2b3b2ca80d0e6b002c
gusikzeyn <huseynzeyn2009@gmail.com>
2024-01-18 19:16:12 +0400

    hasan's second solution

---
+++ minimum and maximum.py
@@ -0,0 +1,13 @@
+def minimum(arr):
+   
+ print (min(arr))
+
+def maximum(arr):
+   
+ print (max(arr))
+
+minimum('759')
+#smallest number in dictionary = 5
+    
+maximum('11289')
+#greatest number in dictionary = 9

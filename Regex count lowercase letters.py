7573a6f9a7f6731fff19caa1402224ee3ee4f5d1
gusikzeyn <huseynzeyn2009@gmail.com>
2024-01-20 14:51:29 +0400

    Regex count lowercase letters

---
+++ Regex count lowercase letters.py
@@ -0,0 +1,6 @@
+def lowercase_count(strng):
+    
+    print(sum(a.islower() for a in strng))
+#this code counts how many letters there are in a given dictionary
+lowercase_count('I am playing football')
+#there are 17 letters in this dictionary

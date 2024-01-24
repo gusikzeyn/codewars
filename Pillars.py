0738fe2807501204fd774f949a130e156c41610b
gusikzeyn <huseynzeyn2009@gmail.com>
2024-01-24 18:53:47 +0400

    Pillars

---
+++ Pillars.py
@@ -0,0 +1,6 @@
+def pillars(num_pill, dist, width):
+    
+    print( dist * 100 * (num_pill - 1) + width * (num_pill - 2) * (num_pill > 1))
+
+pillars(12, 34, 45)
+#number of pillars = 12 dist = 34 width = 45

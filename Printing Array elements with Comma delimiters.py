eeb40a9f980069fc50e3f7af46554cbb8014ed73
gusikzeyn <huseynzeyn2009@gmail.com>
2024-01-30 18:53:45 +0400

    Printing Array elements with Comma delimiters

---
+++ Printing Array elements with Comma delimiters.py
@@ -0,0 +1,8 @@
+def print_array(arr):
+    
+    print( ','.join(map(str, arr)))
+    
+print_array([3,4,35,2,5,25,2,5])    
+#3,4,35,2,5,25,2,5
+print_array("Hello from python")
+#H,e,l,l,o, ,f,r,o,m, ,p,y,t,h,o,

01c885b4e9ed31e97952181aecfccb556d05794b
gusikzeyn <huseynzeyn2009@gmail.com>
2024-02-01 19:50:36 +0400

    For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre

---
+++ For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.py
@@ -0,0 +1,9 @@
+statements = {
+    'george saint pierre': "I am not impressed by your performance.",
+    'conor mcgregor': "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+}
+
+def quote(fighter):
+    print(statements[fighter.lower()])
+quote('conor mcgregor')
+    #I'd like to take this chance to apologize.. To absolutely NOBODY!
